0f38cd44fd95e301d537e50c4b7784073cde5521
nahid-code1 <nahidulislam.work@gmail.com>
2025-08-29 20:00:21 +0600

    added clear button functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const callHistory = [];
+
 // heart count increase function
 const hearts = document.getElementsByClassName("heartNum");
 for (const heart of hearts) {
@@ -20,17 +22,41 @@ for (const call of calls) {
       alert("You don't have enough Coins");
       return;
     }
-    const card = call.closest(".card");
 
+    const card = call.closest(".card");
     const name = card.querySelector(".card-name").innerText.replace(/\n/g, " ");
     const number = card.querySelector(".card-number").innerText;
 
     alert("Calling " + name + "\nNumber: " + number);
-    const totalCoin = coinNumber - 20;
 
+    const totalCoin = coinNumber - 20;
     document.getElementById("coinNum").innerText = totalCoin;
+
+    const data = {
+      serviceName: name,
+      serviceNumber: number,
+      date: new Date().toLocaleTimeString(),
+    };
+
+    callHistory.push(data);
+
+    const hisotyContainer = document.getElementById("call-history");
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="flex justify-between items-center h-20 bg-[#f5fff6] rounded-[12px] my-4 p-4">
+        <div class="">
+          <h1 class="font-semibold text-[16px]">${data.serviceName}</h1>
+          <p class="text-gray-600">${data.serviceNumber}</p>
+        </div>
+        <div class="">${data.date}</div>
+      </div>
+    `;
+    hisotyContainer.appendChild(div);
+
+    console.log(callHistory);
   });
 }
+
 // copy button functionality
 
 const copyButtons = document.querySelectorAll(".btn .fa-copy");
@@ -57,3 +83,11 @@ for (const btnIcon of copyButtons) {
       });
   });
 }
+// clear button
+
+const clearBtn = document.getElementById("clear-btn"); // selects your Clear button
+clearBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("call-history").innerHTML = ""; // remove all call history divs
+  callHistory.length = 0;
+});
